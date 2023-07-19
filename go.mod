1ae0d966a31b501c7aa561c8f9d83c84d491bbb1
Shavit Shelli <shavit.shelli57@gmail.com>
2023-07-19 12:59:29 +0300

    added gorrila http reqres

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/shavitshelli/animals
+
+go 1.20
+
+require github.com/shavitshelli/puppy v0.0.0-20230719091105-a80d6a851654
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/shavitshelli/puppy"
+	"log"
+	"net/http"
+)
+
 func main() {
+	fmt.Println("Hello world")
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+	r := mux.NewRouter()
+	r.HandleFunc("/", servehome).Methods("GET")
+	log.Fatal(http.ListenAndServe(":4000", r))
+
+}
+
+func servehome(w http.ResponseWriter, r *http.Request) {
+	fmt.Println()
+	w.Write([]byte("welcome to golang"))
 
 }
